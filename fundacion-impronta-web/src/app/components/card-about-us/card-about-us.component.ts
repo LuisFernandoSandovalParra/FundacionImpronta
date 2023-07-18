d790e2f4dc5dd52f50139c7f8611937de53e59ca
Luis Sandoval <luchoparrrasandoval@gmail.com>
2023-07-18 18:03:48 -0500

    Se agregó la seccion de fundadores

--- fundacion-impronta-web/src/app/components/card-about-us/card-about-us.component.ts
+++ fundacion-impronta-web/src/app/components/card-about-us/card-about-us.component.ts
@@ -3,8 +3,61 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-about-us',
   templateUrl: './card-about-us.component.html',
-  styleUrls: ['./card-about-us.component.scss']
+  styleUrls: ['./card-about-us.component.scss'],
 })
 export class CardAboutUsComponent {
+  id: string = "";
+  name: string = "";
+  photo: string = "../../../assets/svg/user-default-icon.svg";
+  description: string = "";
 
+  activeCardPersonalInfo: boolean = false;
+  founders = [
+    {
+      id: 'f1',
+      name: 'Admr. Ángela Yohana Molina',
+      photo: '../../../assets/svg/user-default-icon.svg',
+      description:
+        'Administradora y contadora con mas de 10 años de experiencia en el area.'
+    },
+    {
+      id:'f2',
+      name: 'Psic. Laura Natalia Ostos',
+      photo: '../../../assets/svg/user-default-icon.svg',
+      description:
+        'Psicologa especialista con mas de 10 años de experiencia en el area.'
+    },
+    {
+      id:'f3',
+      name: 'Psic. Néstor Ricardo Ávila',
+      photo: "../../../assets/svg/user-default-icon.svg",
+      description:
+        'Psicologo especialista con mas de 15 años de experiencia en el area.'
+    },
+    {
+      id:'f4',
+      name: 'Ing. Cesar Augusto Niño',
+      photo: '../../../assets/svg/user-default-icon.svg',
+      description:
+        'Ingeniero quimico con mas de 15 años de experiencia en el area.'
+    }
+  ];
+
+  constructor() {}
+
+  toogleNamePerson(id:string) {
+    this.toogleCardPerson()
+    for (let i = 0; i < this.founders.length; i++) {
+      if(this.founders[i].id === id){
+        this.id = this.founders[i].id;
+        this.name = this.founders[i].name;
+        this.photo = this.founders[i].photo;
+        this.description = this.founders[i].description
+      }
+    }
+  }
+
+  toogleCardPerson(){
+    this.activeCardPersonalInfo = !this.activeCardPersonalInfo;
+  }
 }
